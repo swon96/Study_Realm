98e584482c60670cda4ab68005d91d6c8e6e72f9
seungwon oh <ohsing0906@gmail.com>
2021-02-26 01:46:18 +0900

    TodoList 완성

--- app/src/main/java/com/linepayjunior/realm_test/MainActivity.kt
+++ app/src/main/java/com/linepayjunior/realm_test/MainActivity.kt
@@ -13,6 +13,7 @@ import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_layout.*
 import kotlinx.android.synthetic.main.dialog_layout.view.*
+import kotlinx.android.synthetic.main.dialog_remove_layout.view.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -31,6 +32,17 @@ class MainActivity : AppCompatActivity() {
         val list_adapter = MainListAdapter(this, title_array)
         realm_listview.adapter = list_adapter
 
+        realm_listview.setOnItemClickListener{ adapterView, view, i, l ->
+
+            removeTodo(realmResult.get(i)!!.id)
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+
         for (i in 0 until realmResult.size){
             title_array.add(realmResult.get(i)!!.title)
         }
@@ -46,9 +58,29 @@ class MainActivity : AppCompatActivity() {
 
                 insertTodo(dialogView.dialog_input.text.toString())
 
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
             }
 
         }
+
+//        removeButton.setOnClickListener{
+//            val builder = AlertDialog.Builder(this)
+//            val dialogView = layoutInflater.inflate(R.layout.dialog_remove_layout, null)
+//
+//            builder.setView(dialogView).show()
+//
+//            dialogView.dialog_remove_button.setOnClickListener{
+//                removeTodo(dialogView.dialog_remove_input.text.toString().toLong())
+//
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            }
+//
+//        }
 
 
     }
@@ -67,10 +99,25 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "insertTodo 실행됨", Toast.LENGTH_LONG).show()
         Log.e("MainActivity realm:" , realmResult.toString())
 
-        // Add 버튼 눌러도 dialog와 화면이 그대로 유지되는 현상을 막기 위하여 MainActivity 를 한번 불러주고 Finish 한다.
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+//        // Add 버튼 눌러도 dialog와 화면이 그대로 유지되는 현상을 막기 위하여 MainActivity 를 한번 불러주고 Finish 한다.
+//        val intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+//        finish()
+
+    }
+
+    private fun removeTodo (id : Long) {
+
+        realm.beginTransaction()
+
+        val deleteItem = realm.where<Todo>().equalTo("id", id).findFirst()
+        deleteItem?.deleteFromRealm()
+
+        realm.commitTransaction()
+
+        Toast.makeText(this,"removeTodo가 실행되었습니다", Toast.LENGTH_LONG).show()
+        Log.e("MainActivity realm:" , realmResult.toString())
+
 
     }
 
